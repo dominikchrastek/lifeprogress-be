4f9a5b151da66a5aff3d9cc29f8ae4012735ebe9
dominik.chrastek <dominik.chrastek@skypicker.com>
2018-03-16 08:01:46 +0100

    add from to params to the getAll wsrecord

--- api/user/wsrecord/getAll.go
+++ api/user/wsrecord/getAll.go
@@ -3,6 +3,7 @@ package wsrecord
 import (
 	"lifeprogress/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
@@ -18,11 +19,52 @@ const getAllSources = `
 	FROM wsr_currency WHERE user_id = $1 and DATE(timestamp) = ANY($2)
 `
 
+// getDateInterval will return []time.Time and these times are between from - to (included these values)
+func getDateInterval(dateFrom string, dateTo string, array []time.Time) ([]time.Time, error) {
+
+	from, err := time.Parse(time.RFC3339, dateFrom)
+	if err != nil {
+		return array, err
+	}
+	// parse toDate to time type
+	to, err := time.Parse(time.RFC3339, dateTo)
+	if err != nil {
+		return array, err
+	}
+
+	for from.Before(to) {
+		array = append(array, from)
+		from = from.Add(time.Hour * 24)
+		if from.Equal(to) {
+			array = append(array, to)
+		}
+	}
+
+	return array, nil
+}
+
 // GetAll route
+// Params:
+// 	id: userId
+//	from: start of interval
+//	to: end of interval
+// records in this interval will be returned
 func (r *Routes) GetAll(c *gin.Context) {
 	var wsources []models.WSRecordGet
 	userID := c.Param("id")
-	dates := pq.Array([]string{"2018-03-16 13:53:50.171906", "2018-03-15 13:55:50.171906"})
+	dateFrom := c.Query("from")
+	dateTo := c.Query("to")
+
+	var arrayTimes []time.Time
+	if dateFrom != "" && dateTo != "" {
+		newArrayTimes, err := getDateInterval(dateFrom, dateTo, arrayTimes)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+		arrayTimes = newArrayTimes
+	}
+
+	dates := pq.Array(arrayTimes)
 
 	if err := r.Db.Select(&wsources, getAllSources, userID, dates); err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
